fac635a7d051874d3847bd88126780bae469d095
Alex <1172068437@qq.com>
2024-11-18 21:32:56 +0800

    build: 添加基于注解管理bean-@Autowired注解-四种方式属性注入 v1.0

--- spring6-ioc-annotation/src/main/java/com/shen/springiocannotation/autowired/controller/UserController.java
+++ spring6-ioc-annotation/src/main/java/com/shen/springiocannotation/autowired/controller/UserController.java
@@ -10,12 +10,35 @@ import org.springframework.stereotype.Controller;
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     //注入Service
-    //第一种方式属性注入-根据属性类型找到对应对象，完成注入 不用构建set方法
-    @Autowired
+    //第一种方式 属性注入-根据属性类型找到对应对象，完成注入 不用构建set方法
+//    @Autowired
+//    private UserService userService;
+
+//    //第二种方式s et方法注入
+//    private UserService userService;
+//
+//    @Autowired
+//    public void setUserService(UserService userService) {
+//        this.userService = userService;
+//    }
+
+////    第三种方式 构造方法注入
+//    private UserService userService;
+//
+//    @Autowired
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+
+    //第四种方式 形参上注入
     private UserService userService;
 
+    public UserController(@Autowired UserService userService) {
+        this.userService = userService;
+    }
+
     public void printHello() {
-        logger.info("控制层调用p打印方法");
+        logger.info("控制层调用打印方法");
         userService.printHello();
     }
 }
