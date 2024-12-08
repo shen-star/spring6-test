89a97c76e3b5db19ee427242aa1fba49d23b7df0
Alex <1172068437@qq.com>
2024-12-08 16:01:24 +0800

    feat: 事务-基于注解的声明式事务-引入事务注解 v1.0

--- spring-jdbc-tx/src/main/java/com/shen/annoation/service/impl/BookServiceImpl.java
+++ spring-jdbc-tx/src/main/java/com/shen/annoation/service/impl/BookServiceImpl.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class BookServiceImpl implements BookService {
     private static final Logger logger = LoggerFactory.getLogger(BookServiceImpl.class);
     @Autowired
